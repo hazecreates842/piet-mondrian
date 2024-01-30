5f08ae733d95f25d71037c5e471b6fd25188dd83
Mary Johnson <mary.johnson@student.norwichuni.ac.uk>
2024-01-30 13:51:50 +0000

    add colour grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
-function setup() {
-    createCanvas(400, 400);
+function setup(){
+
+    const squareSize = 15;
+
+// number of cells of the grid
+    const cellNumber = 15;
+
+
+    const canvasSize = cellNumber * squareSize;
+    createCanvas(canvasSize, canvasSize);
+    background(200)
+
+for(let row = 0; row < cellNumber; row++) {
+    for(let column = 0; column < cellNumber; column++){
+        const red = random(255);
+        const green = random(255);
+        const blue = random(255);
+    
+        const myColour = color(red, green, blue);
+
+    
+        // fill(myColour);
+
+        fill(myColour);
+
+
+        // calculate horizontal position in pixels
+        const x = row * squareSize;
+    
+        // calculate verticle position in pixels
+        const y = column * squareSize;
+
+        const width = squareSize;
+        const height = squareSize;
+
+        square(x, y, width)
+        }
+    }
 }
-  
-function draw() {
-    background(220);
-}
+
